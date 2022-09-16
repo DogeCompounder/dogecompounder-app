7aea3775b237af87824a28f5eededc1bf8ad62e1
DogeCompounder <dogecompounder@gmail.com>
2022-09-16 15:12:24 +0200

    set api url

--- src/features/helpers/getApiInfo.js
+++ src/features/helpers/getApiInfo.js
@@ -1,5 +1,5 @@
-//export const apiUrl = process.env.REACT_APP_API_URL || 'https://api.dogecompounder.dog';
-export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+export const apiUrl = process.env.REACT_APP_API_URL || 'https://api.dogecompounder.dog';
+//export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 // Time-based cache buster
 export const getApiCacheBuster = () => {

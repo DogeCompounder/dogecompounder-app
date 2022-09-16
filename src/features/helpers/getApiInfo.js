218d927d8ddcba5a1e66e909eaa2e2832751dbc4
DogeCompounder <dogecompounder@gmail.com>
2022-09-16 16:15:02 +0200

    Removing /

--- src/features/helpers/getApiInfo.js
+++ src/features/helpers/getApiInfo.js
@@ -1,4 +1,4 @@
-export const apiUrl = process.env.REACT_APP_API_URL || 'https://dogecompounder.herokuapp.com/';
+export const apiUrl = process.env.REACT_APP_API_URL || 'https://dogecompounder.herokuapp.com';
 //export const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 // Time-based cache buster
